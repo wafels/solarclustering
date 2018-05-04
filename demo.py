50e3dec004520ff96f8fe13049908c58f57858db
Jack Ireland <jackireland@gmail.com>
2018-05-04 10:27:14 -0400

    using masks to extract data; plotting

--- demo.py
+++ demo.py
@@ -2,132 +2,203 @@
 # A demo of what might be possible
 #
 import os
-import glob
+from time import time
 
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
+from sklearn import metrics
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import scale
 
 import astropy.units as u
 import sunpy.map
 
+from sklearn.decomposition import PCA
 
-def data_simple_replace_zero_values(data, replacement_value=0.001):
-    """
-    Replace zero values in a numpy array with a fixed replacement value.
-    :param data:
-    :param replacement_value:
-    :return:
-    """
-    return data_simple_replace(data, data == 0, replacement_value)
+# from sunpy.net import Fido, attrs as a
 
+directory = os.path.expanduser('~/Data/solarclustering/demo/1.0/')
+#time_range = a.Time('2012/03/04 12:34:00', '2012/03/04 12:34:12')
+#for wavelength in (94, 131, 171, 193, 211, 335):
+#    result = Fido.search(time_range, a.Instrument('aia'), a.Wavelength(wavelength*u.AA))
+#    downloaded_file = Fido.fetch(result[0, 0], path=os.path.join(directory, '{file}.fits'))
 
-def data_simple_replace_negative_values(data, replacement_value=0.001):
-    """
-    Replace negative values in a numpy array with a fixed replacement value.
-    :param data:
-    :param replacement_value:
-    :return:
-    """
-    return data_simple_replace(data, data < 0, replacement_value)
-
-
-def data_simple_replace_nans(data, replacement_value=0.001):
-    """
-    Replace NaNs in a numpy array with a fixed replacement value.
-    :param data:
-    :param replacement_value:
-    :return:
-    """
-    return data_simple_replace(data, ~np.isfinite(data), replacement_value)
-
-
-def data_simple_replace(data, condition, replacement_value):
-    """
-    Replace values in a numpy array with the replacement value where the input
-    condition is True and return a new array.
-    :param data:
-    :param condition:
-    :param replacement_value:
-    :return:
-    """
-    newdata = deepcopy(data)
-    newdata[condition] = replacement_value
-    return newdata
-
-
-"""
-class ObservationFileInfo:
-    def __init__(self, directory, filename, observation=None):
-        self.directory = directory
-        self.filename = filename
-        self.level = None
-        if observation is not None:
-            self.observation = self._get_observation_from_fileinfo()
-
-        self.filepath = os.path.join(self.directory, self.filename)
-"""
-
-
-#    def _get_observation(self):
-#        """
-#        Finds out what kind of observation is encoded by the filename.
-#        :param filename:
-#        :return:
-#        """
-#        pass
-
-spatial_sum = 4
-n_image_pixels = (4096/spatial_sum) ** 2
-dimensionality = 6
+np.random.seed(42)
+spatial_sum = (2, 2) * u.pix
 
 #
 # Define where the files are
 #
 
-directory = os.path.expanduser('~/Data/solarclustering/demo')
 filenames = sorted([os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.fits')])
-all_data = np.zeroes((n_image_pixels, dimensionality))
-for filename in filenames:
+
+maps = {}
+for i, filename in enumerate(filenames):
     # Get the data
-    data = sunpy.map.Map(filename).superpixel(spatial_sum, spatial_sum).data
+    smap = sunpy.map.Map(filename).superpixel(spatial_sum)
 
-    # Clean the data
-    data = data_simple_replace_nans(data, replacement_value=1.0)
-    data = data_simple_replace_zero_values(data, replacement_value=1.0)
-    data = data_simple_replace_negative_values(data, replacement_value=1.0)
 
-    # Log the data
-    data = np.log(data)
+    # Set up the joint mask on the firt pass through the loop
+    if i == 0:
+        joint_mask = np.zeros_like(smap.data, dtype=np.bool)
 
-    # Central measurement
-    #data_center = np.median(data)
+    # Going to use masks to mask out the bad data
+    # First, mask out the non-finites
+    non_finite_mask = ~np.isfinite(smap.data)
 
-    # Width measurement
-    #data_width = mad(data)
+    # Mask out all the data less than or equal to zero
+    zero_or_less_than_mask = smap.data <= 0
 
-    # Normalize
-    #data = (data - data_center) / data_width
+    # Combine the masks
+    mask = np.logical_or(non_finite_mask, zero_or_less_than_mask)
 
-    data = scale(data)
+    # Masked data
+    masked_data = np.ma.array(smap.data, mask=mask)
 
     # Store
-    these_files.append(data.flatten())
+    maps[smap.measurement] = sunpy.map.Map(masked_data, smap.meta)
+
+    # Joint mask
+    joint_mask = np.logical_or(joint_mask, mask)
+
+    maps[smap.measurement].plot()
+    plt.savefig('/home/ireland/Desktop/map_{:s}.png'.format(str(smap.measurement)))
 
 
+# Normalize the data in each map using the mask in each map
+nmaps = {}
+for key in maps.keys():
+    this_map = maps[key]
+
+    # Transform the data respecting masks
+    log_map_data = np.ma.log(this_map.data)
+
+    # Mean of the data respecting masks
+    mean = np.ma.mean(log_map_data)
+
+    # Standard deviation of the data respecting masks
+    std = np.ma.std(log_map_data)
+
+    # Normalized transformed data
+    nmaps[key] = sunpy.map.Map((log_map_data - mean)/std, this_map.meta)
+
 #
-# Clean up and normalize the data
-# Find where the negatives in each dataset and mask them
+# Plot two-dimensional distributions of the normalized
+# transformed data using the joint mask.  The joint mask
+# indicates where every wavelength has data.
 #
-# How best to normalize the data?.
-# Take log, then define a width measure, either the standard deviation, or
-# median absolute deviation?
-# Then remove mean, mode or median and divide by measurement of width
-#
-#
+keys = list(nmaps.keys())
+nkeys = len(keys)
+bins = [100, 100]
+hrange = [[0, 0], [0, 0]]
+
+# First pass is to get the histogram range so that
+# all the plots have the same scaling.
+maximum_value = -1
+minimum_value = masked_data.size + 1
+for i in range(0, nkeys):
+    key_i = keys[i]
+    skey_i = str(key_i)
+    data1 = np.ma.array(nmaps[key_i].data.data, mask=joint_mask).compressed()
+    for j in range(i+1, nkeys):
+        key_j = keys[j]
+        skey_j = str(key_j)
+        data2 = np.ma.array(nmaps[key_j].data.data, mask=joint_mask).compressed()
+
+        # Calculate the histogram results
+        result = np.histogram2d(data1, data2, bins=bins)
+
+        if result[0].max() > maximum_value:
+            maximum_value = result[0].max()
+
+        if result[0].min() < minimum_value:
+            minimum_value = result[0].min()
+
+        if result[1].min() < hrange[0][0]:
+            hrange[0][0] = result[1].min()
+
+        if result[1].max() > hrange[0][1]:
+            hrange[0][1] = result[1].max()
+
+        if result[2].min() < hrange[1][0]:
+            hrange[1][0] = result[2].min()
+
+        if result[2].max() > hrange[1][1]:
+            hrange[1][1] = result[2].max()
 
 
+# Second pass is to make the plots with the same color scaling.
+for i in range(0, nkeys):
+    key_i = keys[i]
+    skey_i = str(key_i)
+    data1 = np.ma.array(nmaps[key_i].data.data, mask=joint_mask).compressed()
+    for j in range(i + 1, nkeys):
+        key_j = keys[j]
+        skey_j = str(key_j)
+        data2 = np.ma.array(nmaps[key_j].data.data, mask=joint_mask).compressed()
+
+        # Do the plot
+        plt.close('all')
+        plt.hist2d(data1, data2, bins=bins, range=hrange,
+                   norm=colors.PowerNorm(0.25, vmin=minimum_value, vmax=maximum_value),
+                   cmap='plasma')
+        plt.xlabel(key_i)
+        plt.ylabel(key_j)
+        plt.title('Distribution of intensity\nfull frame, transformed, normalized\n{:s} vs. {:s}'.format(skey_i, skey_j))
+        plt.colorbar(label='# pixels ({:n} total)'.format(data2.size))
+        plt.clim(minimum_value, maximum_value)
+        plt.grid('on', linestyle=":")
+        plt.axhline(0.0, color='white', linewidth=0.5)
+        plt.axvline(0.0, color='white', linewidth=0.5)
+        plt.savefig('/home/ireland/Desktop/fullframe_{:s}_{:s}.png'.format(skey_i, skey_j))
+
+plt.close('all')
+
+
+"""
 #
-# Do the cluster analysis
+# Plot two dimensional histograms of the on-disk data only
 #
+for i in range(0, nkeys):
+    # First key
+    key_i = keys[i]
+    skey_i = str(key_i)
+
+    # First map to look at
+    map1 = nmaps[key_i]
+
+    # Calculate a numpy mask indicating where the on disk pixels are
+    map1_pixels_on_disk_mask = ???
+
+    for j in range(i+1, nkeys):
+        # Second key
+        key_j = keys[j]
+        skey_j = str(key_j)
+
+        # Second map to look at
+        map2 = nmaps[key_j]
+
+        # Calculate a numpy mask indicating where the on disk pixels are
+        map2_pixels_on_disk_mask = ???
+
+        # Now calculate a joint mask that shows where the on disk pixels are
+        # and the good data for both datasets simultaneously.
+        map12_disk_mask = np.logical_or(map1_pixels_on_disk_mask, map2_pixels_on_disk_mask)
+        mask = np.logical_or(map12_disk_mask, joint_mask)
+
+        # Get the data
+        data1 = np.ma.array(map1.data.data, mask=mask).compressed()
+        data2 = np.ma.array(map2.data.data, mask=mask).compressed()
+
+        # Plot
+        plt.close('all')
+        plt.hist2d(data1, data2, bins=[100, 100])
+        plt.xlabel(key_i)
+        plt.ylabel(key_j)
+        plt.title('Distribution of intensity\ndisk only, transformed, normalized\n{:s} vs. {:s}'.format(skey_i, skey_j))
+        plt.colorbar()
+        plt.savefig('/home/ireland/Desktop/disk_only_{:s}_{:s}.png'.format(skey_i, skey_j))
+
+plt.close('all')
+"""
